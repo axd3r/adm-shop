4f3d74f573b594efdfe168c86ba851bcdf23fa12
Joaquin <axxdderr@gmail.com>
2025-04-24 01:55:50 -0500

    First commit

---
+++ src/modules/products/dto/create-product.dto.ts
@@ -0,0 +1,36 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    price: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(['men', 'women', 'kid', 'unisex'])
+    @IsString()
+    gender: string;
+
+    @IsString({each: true})
+    @IsArray()
+    @IsOptional()
+    tags?: string[]
+}
